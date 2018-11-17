3a05a32cfe0c467b1e92e207998e9be32fc1e556
james <jameshamm1995@gmail.com>
2018-11-17 20:31:44 +0000

    Left behind change?

--- lol_item_verifier/downloader.py
+++ lol_item_verifier/downloader.py
@@ -1,7 +1,9 @@
 """downloader.py provides a simple way to download the latest data set or
 a specified version.
 
-It will download from the riot api and save it to the data set folder."""
+It will download from the riot api and save it to the data set folder.
+
+It may fail if it cannot reach the url or cannot save locally."""
 from .data import save_data
 
 
